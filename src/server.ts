328153a576540ba5c6f13b710be561ec02ab026e
Jmarchandev <j.marchand.dev@gmail.com>
2021-10-13 00:07:24 +0200

    cors heroku

--- src/server.ts
+++ src/server.ts
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
     "POST, GET, PUT, OPTIONS, DELETE, PATCH"
   );
   res.header(
-    "Access-Control-Allow-Headers",
+    // "Access-Control-Allow-Headers",
     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
   );
   next();
