bc64d59c0d731ebb3b21176aa7cf27e6faaa2c1c
Jmarchandev <j.marchand.dev@gmail.com>
2021-10-13 00:12:37 +0200

    cors heroku

--- src/server.ts
+++ src/server.ts
@@ -41,15 +41,23 @@ const io = new Server(httpServer, {
 app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Credentials", 'true');
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
-    "Access-Control-Allow-Methods",
-    "POST, GET, PUT, OPTIONS, DELETE, PATCH"
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
   );
-  res.header(
-    // "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
+
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Credentials", "true");
+  // res.header(
+  //   "Access-Control-Allow-Methods",
+  //   "POST, GET, PUT, OPTIONS, DELETE, PATCH"
+  // );
+  // res.header(
+  //   // "Access-Control-Allow-Headers",
+  //   "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  // );
   next();
 });
 
