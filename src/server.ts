f9d4d791bb6cfae96b91626d38108a9cd193cc08
Jmarchandev <j.marchand.dev@gmail.com>
2021-10-13 00:26:12 +0200

    cors heroku

--- src/server.ts
+++ src/server.ts
@@ -38,15 +38,15 @@ const io = new Server(httpServer, {
   },
 });
 
-app.use(cors());
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", 'true');
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-//   );
+// app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", 'true');
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
 
   // res.header("Access-Control-Allow-Origin", "*");
   // res.header("Access-Control-Allow-Credentials", "true");
@@ -58,8 +58,8 @@ app.use(cors());
   //   // "Access-Control-Allow-Headers",
   //   "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
   // );
-//   next();
-// });
+  next();
+});
 
 app.use(express.json());
 
