cee06b67dd7ef2c54a34ab625bbdeefd2f810e15
Jmarchandev <j.marchand.dev@gmail.com>
2021-10-13 17:33:58 +0200

    cors heroku

--- src/server.ts
+++ src/server.ts
@@ -44,16 +44,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", 'true');
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-//   );
-//   next();
-// });
 
 app.use(express.json());
 
@@ -61,8 +51,8 @@ const mongoConfig = {
   useNewUrlParser: true,
 };
 
-// const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 io.on("connection", (socket) => {
+  console.log('somewone coming')
   socket.on("connect_error", (err) => {
     console.log(`connect_error due to ${err.message}`);
   });
