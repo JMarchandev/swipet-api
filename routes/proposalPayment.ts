142ad6682efd6ec825d3e8feb414fe0bd26c566d
Jean-Marie MARCHAND <j.marchand.dev@gmail.com>
2022-01-12 13:39:09 +0100

    feat: proposal payment offer

--- routes/proposalPayment.ts
+++ routes/proposalPayment.ts
@@ -1,15 +1,45 @@
 import express, { Request, Response } from "express";
-import { createProposalPayment } from "../controllers/proposalPaymentController";
+import {
+  createOfferProposalPayment,
+  createProposalPayment,
+  getProposalPaymentById,
+} from "../controllers/proposalPaymentController";
 
 const router = express.Router();
 
 router.post("/", async (req: Request, res: Response) => {
-    try {
-        const newProposalPayment = await createProposalPayment(req.body)
-        res.json(newProposalPayment)
-    } catch (error) {
-        res.status(500).json(error);
-    }
+  try {
+    const newProposalPayment = await createProposalPayment(req.body, true);
+    res.json(newProposalPayment);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
+
+router.get("/:proposalPaymentId", async (req: Request, res: Response) => {
+  try {
+    const proposalPayment = await getProposalPaymentById(
+      req.params.proposalPaymentId
+    );
+    res.json(proposalPayment);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post(
+  "/:proposalPaymentId/offer",
+  async (req: Request, res: Response) => {
+    try {
+      const offerProposalPayment = await createOfferProposalPayment(
+        req.params.proposalPaymentId,
+        req.body
+      );
+      res.json(offerProposalPayment);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+);
 
 module.exports = router;
